04bca25652b0649e91a33a2ede727058045856c6
Mrunal <mrunalkumbhare007@gmail.com>
2025-03-26 02:00:32 +0530

    explore HTTP methods

--- src/app.js
+++ src/app.js
@@ -2,16 +2,24 @@ const express = require("express");
 
 const app = express();
 
-app.use("/home",(req,res)=> {
-    res.send("Hello Ji!")
+app.get("/user",(req,res)=> {
+    res.send(`Firstname : "Mrunal", Lastname : "Kumbhare"`)
 })
 
-app.use("/profile",(req,res) => {
-    res.send("Here's all your profile info.")
+app.put("/user",(req,res) => {
+    res.send("Old user replaced with new user instance.")
 })
 
-app.use("/friends",(req,res) => {
-    res.send("Here's the list of all of your friends.")
+app.patch("/user",(req,res) => {
+    res.send("User info updated.")
+})
+
+app.post("/user",(req,res)=> {
+    res.send("New user added.");
+})
+
+app.delete("/user",(req,res)=> {
+    res.send("User deleted.");
 })
 
 app.listen(7777, ()=> {
