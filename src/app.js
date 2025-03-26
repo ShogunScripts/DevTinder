6340f9bbe38237de7841167a8f0a4b0fad1c5473
Mrunal <mrunalkumbhare007@gmail.com>
2025-03-26 18:38:06 +0530

    playing with routes

--- src/app.js
+++ src/app.js
@@ -2,25 +2,66 @@ const express = require("express");
 
 const app = express();
 
-app.get("/user",(req,res)=> {
-    res.send(`Firstname : "Mrunal", Lastname : "Kumbhare"`)
-})
+let rH1 = function (req,res,next) {
+    console.log("Handling the rote path user 1!")
+    // res.send("Response!")
+    next()
+}
+let rH2 = function (req,res,next) {
+    console.log("Handling the rote path user 2!")
+    // res.send("Response!")
+    next()
+}
+let rH3 = function (req,res,next) {
+    console.log("Handling the rote path user 3!")
+    res.send("Response 3!")
+    // next()
+}
+let rH4 = function (req,res,next) {
+    console.log("Handling the rote path user 4!")
+    res.send("Response 4!")
+    // next()
+}
 
-app.put("/user",(req,res) => {
-    res.send("Old user replaced with new user instance.")
-})
+app.get(
+    "/user", rH1, rH2, [rH3, rH4] 
+//     (req,res,next)=> {
+//     console.log("Handling the rote path user!")
+//     // res.send("Response!")
+//     next()
+//     },
+//     (req,res,next)=> {
+//     console.log("Handling the rote path user 2!")
+//     // res.send("Response 2!")
+//     next()
+//     },
+//     (req,res,next)=> {
+//     console.log("Handling the rote path user 3!")
+//     // res.send("Response 2!")
+//     next()
+//     },
+//     (req,res,next)=> {
+//     console.log("Handling the rote path user 4!")
+//     // res.send("Response 2!")
+//     next()
+// }
+)
 
-app.patch("/user",(req,res) => {
-    res.send("User info updated.")
-})
+// app.put("/user",(req,res) => {
+//     res.send("Old user replaced with new user instance.")
+// })
 
-app.post("/user",(req,res)=> {
-    res.send("New user added.");
-})
+// app.patch("/user",(req,res) => {
+//     res.send("User info updated.")
+// })
 
-app.delete("/user",(req,res)=> {
-    res.send("User deleted.");
-})
+// app.post("/user",(req,res)=> {
+//     res.send("New user added.");
+// })
+
+// app.delete("/user",(req,res)=> {
+//     res.send("User deleted.");
+// })
 
 app.listen(7777, ()=> {
     console.log("Server is listening to the port 7777...")
