7799aa5f0d698d82a332c897ab39e9320dac8a5d
Mrunal <mrunalkumbhare007@gmail.com>
2025-03-27 15:21:25 +0530

    create admin auth middleware

--- src/app.js
+++ src/app.js
@@ -1,30 +1,106 @@
 const express = require("express");
+const {userAuth, adminAuth} = require("./middlewares/auth")
 
 const app = express();
 
-let rH1 = function (req,res,next) {
-    console.log("Handling the rote path user 1!")
-    // res.send("Response!")
-    next()
-}
-let rH2 = function (req,res,next) {
-    console.log("Handling the rote path user 2!")
-    // res.send("Response!")
-    next()
-}
-let rH3 = function (req,res,next) {
-    console.log("Handling the rote path user 3!")
-    res.send("Response 3!")
-    // next()
-}
-let rH4 = function (req,res,next) {
-    console.log("Handling the rote path user 4!")
-    res.send("Response 4!")
-    // next()
-}
+// app.use("/user", userAuth)
 
-app.get(
-    "/user", rH1, rH2, [rH3, rH4] 
+// app.use("/user", (req,res,next)=>{
+    // const token = "xyz"
+    // const isUserAuthorized = token === 'xyz'
+    // if (!isUserAuthorized)
+        // res.status(401).send("Unauthorized request.");
+    // else
+        // next();
+// })
+
+// app.use("/admin", (req,res,next) => {
+//     const token = 'qwerty'
+//     const isAdminAuthorized = token === 'qwerty'
+//     if(!isAdminAuthorized)
+//         res.status(401).send("Unauthorized request.")
+//     else
+//         next()
+// })
+
+app.post("/user/login", 
+    // (req,res,next)=>{
+    //     const token = "xyz"
+    //     const isUserAuthorized = token === 'xyz'
+    //     if (!isUserAuthorized)
+    //         res.status(401).send("Unauthorized request.");
+    //     else
+    //         next();
+    // },
+    (req,res) => {
+    res.send("User log-in successful.")
+})
+
+app.get("/user/getData", userAuth,
+    // (req,res,next)=>{
+    //     const token = "xyz"
+    //     const isUserAuthorized = token === 'xyz'
+    //     if (!isUserAuthorized)
+    //         res.status(401).send("Unauthorized request.");
+    //     else
+    //         next();
+    // },
+    (req,res) => {
+    res.send("User's all personal information shown.")
+})
+
+app.get("/admin/getAllData", adminAuth,
+    // (req,res,next) => {
+    // const token = 'qwerty'
+    // const isAdminAuthorized = token === 'qwerty'
+    // if(!isAdminAuthorized)
+    //     res.status(401).send("Unauthorized request.")
+    // else
+    //     next()
+    // },
+    (req,res) => {
+        res.send("All users data fetched")
+    }
+)
+
+app.get("/admin/deleteUser", adminAuth,
+    // (req,res,next) => {
+    //     const token = 'qwerty'
+    //     const isAdminAuthorized = token === 'qwerty'
+    //     if(!isAdminAuthorized)
+    //         res.status(401).send("Unauthorized request.")
+    //     else
+    //         next()
+    // },
+    (req,res) => {
+        res.send("User data deleted.")
+    }
+)
+
+
+// let rH1 = function (req,res,next) {
+//     console.log("Handling the rote path user 1!")
+//     // res.send("Response!")
+//     next()
+// }
+// let rH2 = function (req,res,next) {
+//     console.log("Handling the rote path user 2!")
+//     // res.send("Response!")
+//     next()
+// }
+// let rH3 = function (req,res,next) {
+//     console.log("Handling the rote path user 3!")
+//     res.send("Response 3!")
+//     // next()
+// }
+// let rH4 = function (req,res,next) {
+//     console.log("Handling the rote path user 4!")
+//     res.send("Response 4!")
+//     // next()
+// }
+
+// app.get(
+//     "/user", rH1, rH2, [rH3, rH4] 
 //     (req,res,next)=> {
 //     console.log("Handling the rote path user!")
 //     // res.send("Response!")
@@ -45,7 +121,7 @@ app.get(
 //     // res.send("Response 2!")
 //     next()
 // }
-)
+// )
 
 // app.put("/user",(req,res) => {
 //     res.send("Old user replaced with new user instance.")
---
+++ src/middlewares/auth.js
@@ -0,0 +1,24 @@
+const userAuth = (req,res,next) => {
+    console.log("User authentication is getting checked...")
+    const token = 'xyz';
+    const isAuthorizedUser = token === 'xyz';
+    if(!isAuthorizedUser)
+        res.status(401).send("Unauthorized request")
+    else
+        next()
+}
+
+const adminAuth = (req,res,next) => {
+    console.log("Admin autherization getting checked...")
+    const token = "qwerty"
+    const isAutherizedAdmin = token === "qwerty"
+    if(!isAutherizedAdmin)
+        res.status(401).send("Unauthorized request.")
+    else
+        next()
+}
+
+module.exports = {
+    userAuth,
+    adminAuth,
+}
