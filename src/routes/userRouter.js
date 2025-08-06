482dfdba31eb806c3c55b3dd0b588a17a9768c06
Mrunal <mrunalkumbhare007@gmail.com>
2025-08-06 08:19:00 +0530

    create feed API

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const ConnectionRequest = require("../models/connectionRequest")
 const { userAuth } = require("../middlewares/auth")
+const { User } = require("../models/user")
 
 const userRouter = express.Router()
 
@@ -54,6 +55,51 @@ userRouter.get("/user/connections", userAuth, async (req,res) => {
         res.json({data})
     } catch (err) {
         res.status(400).send({message : err.message})
+    }
+})
+
+userRouter.get("/feed", userAuth, async (req, res)=> {
+    try {
+        const loggedInUser = req.user
+
+        const page = parseInt(req.query.page)
+        let limit = parseInt(req.query.limit) || 10
+        limit = limit>50 ? 50 : limit
+        const skip = (page-1)*limit
+
+        // the feed of user should not include:
+        // 1. himself/herself
+        // 2. users he got rejected from & users he reject
+        // 3. users to whome he sent request & users who sent him request
+        // 4. his own connections
+
+        const connectionRequests = await ConnectionRequest.find({
+            $or : [
+                { fromUserId : loggedInUser._id},
+                { toUserId : loggedInUser._id}
+            ]
+        }).select("fromUserId toUserId")
+
+        const hideUsersFromFeed = new Set()
+
+        connectionRequests.forEach((req)=>{
+            hideUsersFromFeed.add(req.fromUserId.toString())
+            hideUsersFromFeed.add(req.toUserId.toString())
+        })
+
+        const usersInFeed = await User.find({
+            $and : [
+                {_id : {$nin : Array.from(hideUsersFromFeed)}},
+                {_id : {$ne : loggedInUser._id}}
+            ]
+        })
+        .select(USER_SAFE_DATA)
+        .skip(skip)
+        .limit(limit)
+
+        res.json({data : usersInFeed})
+    } catch (err){
+        res.status(400).send("ERROR : "+err.message)
     }
 })
 
